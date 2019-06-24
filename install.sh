e8c9cde6cc7f79c24a64a8ba9e705f2c62f06284
therojam <github@therojam.tech>
2019-06-24 22:57:36 +0200

    added script to automate cloning presentation stuff

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# author: therojam
+# authoremail: github@therojam.tech
+# get ffmd presentation && update submodules with a oneliner
+# 
+
+# 1. clone repo with submodules
+git clone --recursive git@github.com:Jasper-Ben/freifunk-praesentation.git
+
+# 2. update submodules
+cd freifunk-praesentation
+git submodule update
