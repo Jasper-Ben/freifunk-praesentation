47deacfa26f654324d0ea979cf2b9bc2c78adc51
therojam <github@therojam.tech>
2019-07-16 22:22:59 +0200

    Created OS checker after clone + submodule checkout In step 3 this script echos the in stop 0 asked OS and displays a hint for depencies and possible problems while creating the pdf

--- install.sh
+++ install.sh
@@ -4,13 +4,25 @@
 # get ffmd presentation && update submodules with a oneliner
 # 
 
+macos_hint="Since you are using macOS, please remind the dependencies as described in the README.md and check your \$PATH!"
+linux_hint="Please remind the dependencies as described in the README.md and have fun!"
+
 # 0. check for OS
-# system 
-# 
-# 
+system=$(uname)
+
 # 1. clone repo with submodules
 git clone --recursive git@github.com:Jasper-Ben/freifunk-praesentation.git
 
 # 2. update submodules
 cd freifunk-praesentation
 git submodule update
+
+# 3. macos users get a hint for $PATH and MacTeX
+if ($system == Darwin); then
+	echo $macos_hint
+else
+	echo $linux_hint
+fi
+
+# @Test
+# vim: ai et sts=4 ts=4 sw=4
